4a640647c67fc7bf8f100206a2497b256a9b8104
Gourav Sharma <sgourav533@gmail.com>
2021-08-29 03:09:15 +0530

    added spring data repository, closes #39.

---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/repositories/PetTypeRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.petclinic.repositories;
+
+import com.springframework.petclinic.model.PetType;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PetTypeRepository extends CrudRepository<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.petclinic.repositories;
+
+import com.springframework.petclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+}
