5909f62f754acbb47044b7e6bad4df347c356af2
Gourav Sharma <sgourav533@gmail.com>
2021-08-29 01:22:34 +0530

    impl Base Entity update id , closes #35.

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/BaseEntity.java
@@ -1,9 +1,17 @@
 package com.springframework.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+//it will be inherited by other no table needed for this
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
