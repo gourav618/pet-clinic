3ba3e9146ee08329d797b73910fa505e8762318b
Gourav Sharma <sgourav533@gmail.com>
2021-08-30 02:06:07 +0530

    added project lombok and refactored, closes #51.

--- pet-clinic-data/src/main/java/com/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/model/Owner.java
@@ -1,9 +1,17 @@
 package com.springframework.petclinic.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 //we want table for it
 @Entity
 @Table(name = "owners")
@@ -21,35 +29,14 @@ public class Owner extends Person{
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        super(id,firstName,lastName);
         this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
         this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
         this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
         this.pets = pets;
     }
+
 }
